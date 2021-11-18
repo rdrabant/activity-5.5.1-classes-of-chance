563789c9a8b26c9952f5e71ddfa655f6336daf07
Robert Drabant <rdrabant@resultsdelivered.com>
2021-11-17 19:40:38 -0800

    initial build

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
 class Casino {
     // Write code here
+    
+   
+
+    constructor(casinoName, isFakeCoin = false){
+        this.casinoName = casinoName;
+        this.timesPlayed = 0;
+        this.isFakeCoin = isFakeCoin;
+    }
+
+    playGame(betAmount){
+        
+        if(Math.random() <= 0.5 || this.isFakeCoin){
+            console.log("Casino Wins");
+        }else{
+            this.timesPlayed++;
+            
+            let amount = betAmount * (this.timesPlayed);
+        
+            console.log("You won " + amount + " from ${this.casinoName}");
+            
+        }
+
+    }
+
+    
+    rollDie(die){
+        die++; //so we can get floor
+        console.log("You rolled a " +  Math.floor(Math.random() * die));
+        
+    }
+
+
 };
 
 // TESTS
@@ -11,20 +43,18 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
-/*
 const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
 console.log(myExtraBonusCasino);
 myExtraBonusCasino.rollDie(6);
 myExtraBonusCasino.rollDie(20);
 myExtraBonusCasino.rollDie(100);
-*/
+
